a2015ab8c10c2ad782d028fabbef22f3997436e3
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2024-11-18 14:03:03 -0500

    Began gameOver.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowseRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+import Home from "./hosting/src/app.jsx";
+import gameOver from "./hosting/src/gameOver.jsx";
+import playScreen from "./hosting/src/playScreen.jsx";
+import winScreen from "./hosting/src/winScreen.jsx";
+
+
+
 
 function App() {
   return (
     <div className="App">
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/gameOver" element={<gameOver />} />
+        <Route
+            path="/playScreen"
+            element={<playScreen />}
+        />
+        <Route path="/winScreen" element={<winScreen />} />
+    </Routes>
+    </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
