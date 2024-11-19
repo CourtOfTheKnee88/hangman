e77bb30b29b496ff4fb6f935b0b6d771465a881d
Courtney Jackson <courtneymjackson8@gmail.com>
2024-11-19 12:25:01 -0500

    Update App.jsx
    Start of button selection. Hover/active colors need to be update to be more distinctive.

--- hosting/src/App.jsx
+++ hosting/src/App.jsx
@@ -3,6 +3,17 @@ import './App.css'
 
 function App() {
 
+  const [wordDifficulty, setWordDifficulty] = useState('')
+  const [pictureDifficulty, setPictureDifficulty] = useState('')
+
+  // function startGame() {
+  //   if (wordDifficulty === '' || pictureDifficulty === '') {
+  //     alert('Please select both word and picture difficulty')
+  //   } else {
+  //     window.location.href = `playScreen?wordDifficulty=${wordDifficulty}&pictureDifficulty=${pictureDifficulty}`
+  //   }
+  // }
+
   return (
     <div className='h-screen flex justify-center items-center'>
       <div className='flex flex-col justify-center items-center rounded-xl shadow-xl w-[40%] bg-slate-100 p-2'>
@@ -12,23 +23,35 @@ function App() {
           <div className='flex flex-col'>
             <p className='font-semibold pb-2 text-lg text-center'>Word Difficulty:</p>
             <div className='flex justify-center space-x-2'>
-              <button className='bg-green-500 text-white rounded-md px-2'>Easy</button>
-              <button className='bg-yellow-400 text-white rounded-md px-2'>Medium</button>
-              <button className='bg-red-600 text-white rounded-md px-2'>Hard</button>
+              <button 
+                onClick={() => setWordDifficulty('easy')} 
+                className={`${wordDifficulty === 'easy' ? 'bg-green-700' : 'bg-green-500'} text-white rounded-md px-2 hover:bg-green-700`}>Easy</button>
+              <button 
+                onClick={() => setWordDifficulty('medium')}
+                className={`${wordDifficulty === 'medium' ? 'bg-yellow-500' : 'bg-yellow-400'} bg-yellow-400 text-white rounded-md px-2 hover:bg-yellow-500`}>Medium</button>
+              <button 
+                onClick={() => setWordDifficulty('hard')} 
+                className={`${wordDifficulty === 'hard' ? 'bg-red-700' : 'bg-red-600'} bg-red-600 text-white rounded-md px-2 hover:bg-red-700`}>Hard</button>
             </div>
           </div>
 
           <div className='flex flex-col'>
             <p className='font-semibold pb-2 text-lg text-center'>Picture Difficulty:</p>
             <div className='flex justify-center space-x-2'>
-              <button className='bg-green-500 text-white rounded-md px-2'>Easy (9 tries)</button>
-              <button className='bg-yellow-400 text-white rounded-md px-2'>Medium (6 tries)</button>
-              <button className='bg-red-600 text-white rounded-md px-2'>Hard (4 tries)</button>
+              <button 
+                onClick={() => setPictureDifficulty('easy')}
+                className={`${pictureDifficulty === 'easy' ? 'bg-green-700' : 'bg-green-500'} text-white rounded-md px-2 hover:bg-green-700`}>Easy (9 tries)</button>
+              <button 
+                onClick={() => setPictureDifficulty('medium')} 
+                className={`${pictureDifficulty === 'medium' ? 'bg-yellow-500' : 'bg-yellow-400'} bg-yellow-400 text-white rounded-md px-2 hover:bg-yellow-500`}>Medium (6 tries)</button>
+              <button 
+                onClick={() => setPictureDifficulty('hard')} 
+                className={`${pictureDifficulty === 'hard' ? 'bg-red-700' : 'bg-red-600'} bg-red-600 text-white rounded-md px-2 hover:bg-red-700`}>Hard (4 tries)</button>
             </div>
           </div>
         </div>
         <div className='py-6'>
-          <button className='rounded-md bg-blue-500 text-white px-2 text-2xl'>Start</button>
+          <button onClick={() => startGame()} className='rounded-md bg-blue-500 hover:bg-blue-700 text-white px-2 text-2xl'>Start</button>
         </div>
       </div>
     </div>
